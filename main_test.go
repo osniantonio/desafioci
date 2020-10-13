0df5ff546018710cbb98abf5e32792444b2a6b4c
osni.antonio <osni.antonio@gmail.com>
2020-10-12 21:56:27 -0300

    Desafio CI primeira versão

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-    total := Soma(5, 5)
+    total := Soma(5, 1)
     if total != 10 {
        t.Errorf("Soma incorreta, valor: %d, esperado: %d.", total, 10)
     }
